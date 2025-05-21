3c9fb79d52324c6ae9cc5e7c59441b90a3e1a6d6
tazamaestra <maestrataza@gmail.com>
2025-05-21 12:44:50 -0500

    configurando postgres

--- libs/postgresPool.js
+++ libs/postgresPool.js
@@ -1,23 +1,23 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
+/*const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   host: process.env.POSTGRES_HOST,
   port: process.env.POSTGRES_PORT,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
-});
+});*/
 
-/*const { Pool } = require('pg');
+const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false, // Necesario en Heroku
   },
 });
-*/
+
 
 
 module.exports = pool;
