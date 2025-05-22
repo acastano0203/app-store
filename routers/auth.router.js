0c5c014797379ecb9dd21fc8e5a8ebcf8360aef3
tazamaestra <maestrataza@gmail.com>
2025-05-22 09:07:48 -0500

    update auth

--- routers/auth.router.js
+++ routers/auth.router.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const { loginUser } = require("../services/auth.service.js");
 
 router.post("/", async (req, res) => {
-  const { username, password, id } = req.body;
+  const { username, password } = req.body;
   try {
-    const result = await loginUser(username, password, id);
+    const result = await loginUser(username, password);
     res.json(result);
   } catch (err) {
     res.status(401).json({ error: err.message });
